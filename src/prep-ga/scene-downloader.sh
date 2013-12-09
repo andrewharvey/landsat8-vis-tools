b9059b1cfaef55517fb87a8d94bc2dd105cd8cab
Andrew Harvey <andrew.harvey4@gmail.com>
2013-12-09 22:33:44 +1100

    add license text within source file

--- src/prep-ga/scene-downloader.sh
+++ src/prep-ga/scene-downloader.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This script is licensed CC0 by Andrew Harvey <andrew.harvey4@gmail.com>
+#
+# To the extent possible under law, the person who associated CC0
+# with this work has waived all copyright and related or neighboring
+# rights to this work.
+# http://creativecommons.org/publicdomain/zero/1.0/
+
 scene=$1
 output_dir=$2
 
